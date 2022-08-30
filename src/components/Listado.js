61aa78d3e5d14becbd63fec81e07b451ab47afe9
NataliaOrtizMa <nataliaortizma@gmail.com>
2022-08-30 13:25:03 -0500

    Desarrollo del funcionamiento de los componentes

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import data from "./data.json";
+import Item from './Item';
 
 // Debemos importar el listado de objetos del archivo JSON para usarlos como array a la hora de crear cada Item.
 // El componente Listado es el padre de:
@@ -7,9 +9,14 @@ import React from 'react'
 // MÉTODOS: Listado no requiere de métodos.
 // PROPS: Listado recibe el método para aumentar el estado de App y se lo pasa a cada uno de sus hijos.
 
-export default function Listado() {
+export default function Listado(props) {
+
+  const { handleCarrito } = props;
+  
   return (
     <div className='container'>
+      {data.map((product) => 
+        <Item key={product.id} producto={product.producto} stock={product.stock} handleCarrito={handleCarrito}></Item>)}
       {/* renderizamos los Item aquí */}
     </div>
   )
